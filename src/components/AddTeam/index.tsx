7d13b08a43aee40719057df3f35a593de8f7b163
Ioan Latrovaliev <latrovalievioan@gmail.com>
2024-07-08 09:21:02 +0300

    kinda fix dialog

--- src/components/AddTeam/index.tsx
+++ src/components/AddTeam/index.tsx
@@ -1,20 +1,33 @@
-import { createSignal } from "solid-js";
 import PlusIcon from "~/assets/PlusIcon";
 import Modal from "../Modal";
+import { createEffect } from "solid-js";
 
 export default function AddTeam() {
-  const [isModalOpen, setIsModalOpen] = createSignal(false);
+  let modalRef!: HTMLDialogElement;
+
+  createEffect(() => {
+    modalRef.close();
+  });
 
   return (
     <>
       <button
         class="sketched-border-1 flex w-max gap-2 border-2 border-stone-800 p-2 shadow-md shadow-stone-800 hover:shadow-lg hover:shadow-stone-800"
-        onClick={() => setIsModalOpen(true)}
+        onClick={() => {
+          modalRef.showModal();
+        }}
       >
         <PlusIcon />
         Add team
       </button>
-      {isModalOpen() ? <Modal closeModal={() => setIsModalOpen(false)}>go6o</Modal> : null}
+      <Modal ref={modalRef}>
+        <div class="sketched-line flex w-full border-b-4 border-stone-800">
+          <h1>Add team:</h1>
+          <button onClick={() => modalRef.close()} class="flex-1">
+            x
+          </button>
+        </div>
+      </Modal>
     </>
   );
 }
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,25 +1,16 @@
 import { ParentProps } from "solid-js";
-import { Portal } from "solid-js/web";
 
 type Props = {
-  closeModal: () => void;
+  ref: HTMLDialogElement;
 };
 
 export default function Modal(props: ParentProps<Props>) {
   return (
-    <Portal>
-      <div class="fixed inset-0 z-50 flex items-center justify-center">
-        <div
-          class="absolute inset-0 backdrop-blur-sm"
-          onClick={props.closeModal}
-        />
-          <dialog
-            class="sketched-border-1 relative min-w-96 rounded border-4 border-stone-800 p-4 bg-amber-100"
-            open
-          >
-            {props.children}
-          </dialog>
-      </div>
-    </Portal>
+    <dialog
+      class="sketched-border-1 relative flex min-w-96 flex-col gap-4 rounded border-4 border-stone-800 bg-amber-100 p-4"
+      ref={props.ref}
+    >
+      {props.children}
+    </dialog>
   );
 }
